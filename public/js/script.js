33cca0b66c9a197c8e07fe0d194253847b124d78
adhilameenet <adhilameenet@gmail.com>
2023-03-07 08:40:12 +0530

    temp5

--- public/js/script.js
+++ public/js/script.js
@@ -2,6 +2,6 @@
 // Remove Alert Message
 
 const alertMessage = document.querySelector(".alert-delete");
-alertMessage.addEventListener("click", () => {
+alertMessage.addEventListener("click", function(){
   alertMessage.classList.add("hidden");
 });
